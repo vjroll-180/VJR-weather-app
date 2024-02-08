74b7c687773313f02e2561aa650f38257421d7a3
vjroll-180 <vjroll180@gmail.com>
2024-02-08 10:57:17 +0000

    Add basic search engine (no API)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function submitSearch(event) {
+  event.preventDefault();
 
+  let searchInput = document.querySelector("#form-input-search");
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", submitSearch);
