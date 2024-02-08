9dca61ed6404bbaf32bfe440bc77bbd7bb36621e
vjroll-180 <vjroll180@gmail.com>
2024-02-08 18:48:13 +0000

    Added icon via JS using API

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ function refreshWeather(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   timeElement.innerHTML = formatDate(date);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img
+              src="${response.data.condition.icon_url}" class="weather-data-current-icon"/>`;
 }
 
 function formatDate(date) {
