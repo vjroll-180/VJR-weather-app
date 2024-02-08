e280ff332d14c0efbc877c36777913fd1aac0798
vjroll-180 <vjroll180@gmail.com>
2024-02-08 12:43:06 +0000

    Added basic search engine functionality using API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-function submitSearch(event) {
-  event.preventDefault();
-
-  let searchInput = document.querySelector("#form-input-search");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
 
-  cityElement.innerHTML = searchInput.value;
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "13da71b4bebee18bac0t70ddd06fbo9b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#form-input-search");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
+
+searchCity("Lisbon");
